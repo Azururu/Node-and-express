e0b7cbf0f11281951523beba3d892fceb4019ef7
Azuru <67322429+Azururu@users.noreply.github.com>
2025-11-04 11:02:17 +0200

    add cat api, add public folder

--- app.js
+++ app.js
@@ -3,9 +3,20 @@ const hostname = '127.0.0.1';
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Welcome to my REST API!');
-});
+const cat = {
+  cat_id: 1,
+  name: 'mr. meow',
+  birthdate: '15-11-2020',
+  weight: 4,
+  owner: 'Geralt',
+  image: 'https://loremflickr.com/320/240/cat'
+}
+
+app.use('/public', express.static('public'));
+
+app.get('/api/v1/cat', (request, response) => {
+  response.send(cat);
+})
 
 app.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
